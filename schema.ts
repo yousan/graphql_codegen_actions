a0e0322a2858559c39ae3228c42768c9f8f58188
yousan <yousan@l2tp.org>
2021-02-05 17:38:42 +0900

    init

---
+++ schema.ts
@@ -0,0 +1,45 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export interface Scalars {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  AWSDate: any;
+  AWSTime: any;
+  AWSDateTime: any;
+  AWSTimestamp: any;
+  AWSEmail: any;
+  AWSJSON: any;
+  AWSURL: any;
+  AWSPhone: any;
+  AWSIPAddress: any;
+  BigInt: any;
+  Double: any;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export interface IfCharacter {
+  __typename?: 'Character';
+  name: Scalars['String'];
+}
